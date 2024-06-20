2958c4d20e1db91d27d3b2443200df67c70360c8
Aniede Chibundu <ndubest56@gmail.com>
2024-06-20 12:56:29 +0100

    get a single user

--- src/app/api/todo/[id]/route.ts
+++ src/app/api/todo/[id]/route.ts
@@ -3,11 +3,17 @@ import { NextResponse } from "next/server";
 const API_KEY: string = process.env.DATA_API_KEY as string;
 const DATA_SOURCE_URL = `https://jsonplaceholder.typicode.com/todos`;
 
-export async function GET(request: Request) {
-  const id = request.url.slice(request.url.lastIndexOf(`/`) + 1);
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+//get the id from the second argument {params} instead of slicing the string
+export async function GET(request: Request, { params: { id } }: Props) {
+  //   const id = request.url.slice(request.url.lastIndexOf(`/`) + 1);
 
   const resp = await fetch(`${DATA_SOURCE_URL}/${id}`);
-
   const todoData: Todo = await resp.json();
   if (!todoData.id) {
     return NextResponse.json({
