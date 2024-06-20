f2bfc51221b0d2fff868f6ad9528a87c55af73ee
Aniede Chibundu <ndubest56@gmail.com>
2024-06-20 10:01:56 +0100

    Delete Api

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 const DATA_SOURCE_URL = `https://jsonplaceholder.typicode.com/todos`;
+const API_KEY: string = process.env.DATA_API_KEY as string;
 
 export async function GET() {
   const resp = await fetch(DATA_SOURCE_URL);
@@ -8,4 +9,22 @@ export async function GET() {
   const todos: Todo[] = await resp.json();
 
   return NextResponse.json(todos);
+}
+
+export async function DELETE(request: Request) {
+  const { id }: Partial<Todo> = await request.json();
+
+  // check for id
+  if (!id) return NextResponse.json({ Message: `Togo id required` });
+
+  // delete the togo from the api source
+  const resp = fetch(`${DATA_SOURCE_URL}/${id}`, {
+    method: `DELETE`,
+    headers: {
+      "Content-Type": "application/json",
+      "API-Key": API_KEY,
+    },
+  });
+  console.log(`Deleted`, resp);
+  return NextResponse.json({ Message: `Todo ${id} Deleted` });
 }
