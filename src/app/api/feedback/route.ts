407c5c1e593e25931c90deba2c954c31e58550c3
Aniede Chibundu <ndubest56@gmail.com>
2024-06-20 07:47:17 +0100

    post response success

--- src/app/api/feedback/route.ts
+++ src/app/api/feedback/route.ts
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
 
 type Feedback = {
-  name: string;
-  email: string;
-  message: string;
+  name?: string;
+  email?: string;
+  message?: string;
 };
 
 export async function POST(request: Request) {
   const result: Feedback = await request.json();
-  console.log(result);
+  console.log(`THE RESULT`, result);
   return NextResponse.json(result);
 }
--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -1,33 +1,39 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
-const initialState = {
-  name: ``,
-  email: ``,
-  message: ``,
-};
+// const initialState = {
+//   name: ``,
+//   email: ``,
+//   message: ``,
+// };
 
 export default function page() {
-  const [data, setData] = useState(initialState);
+  //   const [data, setData] = useState(initialState);
+  const [name, setName] = useState(``);
+  const [email, setEmail] = useState(``);
+  const [message, setMessage] = useState(``);
+
   const router = useRouter();
+
+  const data = { name, email, message };
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(JSON.stringify(data));
-    const { name, email, message } = data;
+    // const { name, email, message } = data;
     const res = await fetch(`http://localhost:3000/api/feedback`, {
       method: "Post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     });
     const result = await res.json();
-    console.log(result);
+    console.log(`THE CLIENT`, result);
   };
   return (
     <div className="w-full bg-slate-600 h-screen text-white/70">
-      <form className="mx-auto w-[60%] pt-10">
+      <form onSubmit={handleSubmit} className="mx-auto w-[60%] pt-10">
         <h2 className="text-2xl ">Contact us</h2>
         <div className="flex flex-col gap-3">
           <div className="flex flex-col">
@@ -36,6 +42,10 @@ export default function page() {
               type="text"
               placeholder="Name"
               className="rounded-2xl p-3 px-6"
+              name="name"
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setName(e.target.value)
+              }
             />
           </div>
           <div className="flex flex-col">
@@ -43,17 +53,29 @@ export default function page() {
             <input
               type="email"
               placeholder="Email"
-              className="rounded-2xl p-3 px-6"
+              name="email"
+              className="rounded-2xl p-3 px-6 "
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setEmail(e.target.value)
+              }
             />
           </div>
           <div className="flex flex-col">
             <label className="m-2 mx-3">Message</label>
             <textarea
-              className="rounded-2xl p-3 px-6 h-20"
+              className="rounded-2xl p-3 px-6 h-[100px] text-black"
               placeholder="write your message"
+              name="message"
+              onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+                setMessage(e.target.value)
+              }
             />
           </div>
         </div>
+        <input
+          type="submit"
+          className="rounded-2xl p-3 px-6 bg-blue-600 my-6 w-full cursor-pointer"
+        />
       </form>
     </div>
   );
