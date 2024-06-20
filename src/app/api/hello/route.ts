27ebba0f6d1607a9974fa5a497aea3b2567a846a
Aniede Chibundu <ndubest56@gmail.com>
2024-06-20 17:28:06 +0100

    limit number of access

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,10 +1,15 @@
 import { NextResponse } from "next/server";
 import { limiter } from "../config/limiter";
 
-export async function GET() {
+// use request to get the origin of the reques... optional
+export async function GET(request: Request) {
   const remaining = await limiter.removeTokens(1);
 
+  // get the origin of request
+  const origin = request.headers.get("origin");
   if (remaining < 0) {
+    //using the new NextResponse is optional
+    //you can just display the message in console or as a response json
     return new NextResponse(null, {
       status: 403,
       statusText: `Too many request`,
